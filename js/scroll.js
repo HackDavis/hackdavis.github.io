7ec460278517bebc125966eef5577214b59c2b5b
klanmiko <eternity01033@gmail.com>
2017-08-14 10:35:34 -0700

    floating clouds animation

--- js/scroll.js
+++ js/scroll.js
@@ -1,5 +1,10 @@
 $(function() {
-    var controller = new ScrollMagic.Controller();
+    $("#left-cloud1").velocity({left: "3000px"}, {duration: 100000, easing:"linear"});
+    $("#left-cloud2").velocity({left: "3000px"}, {duration: 100000, easing:"linear"});
+    $("#left-cloud3").velocity({left: "3000px"}, {duration: 100000, easing:"linear"});
+    $("#right-cloud1").velocity({right: "3000px"}, {duration: 100000, easing:"linear"});
+    $("#right-cloud2").velocity({right: "3000px"}, {duration: 100000, easing:"linear"});
+    /*var controller = new ScrollMagic.Controller();
     var poofLeftCloud = new ScrollMagic.Scene({
         offset:0,
         duration: 150
@@ -29,5 +34,5 @@ $(function() {
         duration: 300
     })
     .setTween("#left-cloud3", {left: "-200px"})
-    .addTo(controller);
+    .addTo(controller);*/
 });

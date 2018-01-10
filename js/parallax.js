ad49a961aaf2ca1b2a21a806ba77403ea12d731f
Kaelan Mikowicz <eternity01033@gmail.com>
2018-01-09 18:47:28 -0800

    remove the typeform from the lantern

--- js/parallax.js
+++ js/parallax.js
@@ -111,17 +111,7 @@ $("#clickFAQ").click(function(){
 });
 var closeTimeout;
 /* Apply button */
-$("#lantern,#apply-button").click(function(){
-  if(window.innerWidth <= 750) {
-    window.location.href="./apply.html";
-  }
-  $('html').css('overflow', 'hidden');
-  $("#close").toggleClass("hid");
-  $("#typeform").toggleClass("hid");
-  $("#typeform").focus();
-  //$("#close").velocity({top: '10px'});
-  //$('#typeform').velocity({top: 0});
-});
+
 $("#close").click(function(){
   //$("#close").velocity({top: "100%"});
   //$("#typeform").velocity({top: "100%"});
